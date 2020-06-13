9a06b557a9a910562cfaed8c6d196f70035391cb
Łukasz Cydejko <johnatanherbatka@gmail.com>
2020-06-13 20:26:59 +0200

    Dodaj pliki projektów.

---
+++ ItemTemplate/SecendPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ItemTemplate
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class SecendPage : Page
+    {
+        private ObservableCollection<model> Models;
+        private model selectedModel;
+        public SecendPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Models = (ObservableCollection<model>) e.Parameter;
+        }
+
+        private void listaCar_GridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedModel = (model)listaCar_GridView.SelectedItem;
+            ObrazAuta_Image.Source = new BitmapImage(new Uri(selectedModel.Obrazek));
+        }
+
+        private void usun()
+        {
+            if (selectedModel != null)
+            {
+                Models.Remove((model)listaCar_GridView.SelectedItem);
+            }
+        }
+
+        private void dodaj(string nazwa, int cena, string obraz)
+        {
+            model nowySamochod = new model(nazwa, cena, obraz);
+            Models.Add(nowySamochod);
+        }
+    }
+}
---
+++ ItemTemplate/model.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemTemplate
+{
+    public class model : INotifyPropertyChanged
+    {
+        string nazwa;
+        int cena;
+        string obrazek;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public model(string nazwa, int cena, string obrazek)
+        {
+            Nazwa = nazwa;
+            Cena = cena;
+            Obrazek = obrazek;
+        }
+
+        public string Nazwa
+        {
+            get => nazwa; 
+            set
+            {
+                nazwa = value;
+                RaisePropertyChanged("Nazwa");
+            }
+        }
+        public int Cena
+        {
+            get => cena; 
+            set
+            {
+                cena = value;
+                RaisePropertyChanged("Cena");
+            }
+        }
+        public string Obrazek
+        {
+            get => obrazek; 
+            set
+            {
+                obrazek = value;
+                RaisePropertyChanged("Obrazek");
+            }
+        }
+
+    }
+
+}
